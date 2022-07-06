4b3b812532516c81cb2c08f976db7a28d2752a70
Gaurav Jha <80048862+GAURAV5925@users.noreply.github.com>
2022-07-06 10:48:21 +0530

    Update polymorphism.cpp

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -12,7 +12,7 @@ class Geeks
     cout << "value of x is " << x << endl;
   }
   
-  // function with same name but 1 double parameter
+  // function with same name but 1 double paramete
   void func(double x)
   {
     cout << "value of x is " << x << endl;
